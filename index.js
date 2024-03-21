c947b384b696b9fa28caf95d538eb93412743aec
Ренат Парастаев <renatparastaev@bk.ru>
2024-03-21 19:10:12 +0300

    intermediate

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelector('.link').addEventListener('click', () => {
         if (navigator.userAgent.includes("iPhone")) {
-            alert('pizdec')
-            window.location.href = "sberbankonline://payments/p2p-by-phone-number?phoneNumber=+79652259554";
-            setTimeout(() => window.location.href = "sbolonline://payments/p2p-by-phone-number?phoneNumber=+79652259554", 100);
-            setTimeout(() => window.location.href = "intent://ru.sberbankmobile/payments/p2p?type=phone_number&phoneNumber=+79652259554", 200);
+            window.location.href = "sbolonline://payments/p2p-by-phone-number?phoneNumber=+79889368516";
+            setTimeout(() => window.location.href = "sberbankonline://payments/p2p-by-phone-number?phoneNumber=+79889368516", 100);
         } else {
-            window.location.href = "intent://ru.sberbankmobile/payments/p2p?type=phone_number&phoneNumber=+79652259554"
+            window.location.href = "intent://ru.sberbankmobile/payments/p2p?type=phone_number&phoneNumber=+79889368516"
         }
     });
 });
