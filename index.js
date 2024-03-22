f5f2ce9a09e1adb35a0d43f3793eac255ec74f91
Ренат Парастаев <renatparastaev@bk.ru>
2024-03-22 15:50:46 +0300

    intermediate

--- index.js
+++ index.js
@@ -2,15 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const link = document.getElementById('link');
     const qr = document.getElementById('qr');
-
-    link.addEventListener('click', () => {
-        if (navigator.userAgent.includes("iPhone")) {
-            window.location.href = "sbolonline://payments/p2p-by-phone-number?phoneNumber=+79889368516";
-            setTimeout(() => window.location.href = "sberbankonline://payments/p2p-by-phone-number?phoneNumber=+79889368516", 100);
-        } else {
-            window.location.href = "intent://ru.sberbankmobile/payments/p2p?type=phone_number&phoneNumber=+79889368516"
-        }
-    });
     const isMobile = navigator.userAgentData.mobile;
 
     alert(isMobile);
@@ -20,4 +11,13 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         link.remove();
     }
+
+    link.addEventListener('click', () => {
+        if (navigator.userAgent.includes("iPhone")) {
+            window.location.href = "sbolonline://payments/p2p-by-phone-number?phoneNumber=+79889368516";
+            setTimeout(() => window.location.href = "sberbankonline://payments/p2p-by-phone-number?phoneNumber=+79889368516", 100);
+        } else {
+            window.location.href = "intent://ru.sberbankmobile/payments/p2p?type=phone_number&phoneNumber=+79889368516"
+        }
+    });
 });
